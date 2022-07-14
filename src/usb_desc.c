187b9072e3de246bbe188cb7a4e36f929652cad3
Christopher Holley <torchtopher@gmail.com>
2022-07-14 14:08:49 -0400

    Working 8 bit axis

--- src/usb_desc.c
+++ src/usb_desc.c
@@ -280,6 +280,7 @@ static uint8_t mouse_report_desc[] = {
 
 #ifdef JOYSTICK_INTERFACE
 #if JOYSTICK_SIZE == 12
+
 static uint8_t joystick_report_desc[] = {
         0x05, 0x01,                     // Usage Page (Generic Desktop)
         0x09, 0x04,                     // Usage (Joystick)
@@ -292,36 +293,31 @@ static uint8_t joystick_report_desc[] = {
         0x19, 0x01,                     //   Usage Minimum (Button #1)
         0x29, 0x20,                     //   Usage Maximum (Button #32)
         0x81, 0x02,                     //   Input (variable,absolute)
+        0x05, 0x01,                     //   Usage Page (Generic Desktop)
+        0x09, 0x01,                     //   Usage (Pointer)
+        0xA1, 0x00,                     //   Collection ()
+        0x15, 0x00,                     //     Logical Minimum (0)
+        0x26, 0xFF, 0x00,               //     Logical Maximum (255)
+        0x75, 0x08,                     //     Report Size (8)
+        0x95, 0x06,                     //     Report Count (6)
+        0x09, 0x30,                     //      Usage (X)
+        0x09, 0x31,                     //      Usage (Y)
+        0x09, 0x32,                     //      Usage (Z)
+        0x09, 0x33,                     //      Usage (Rx)
+        0x09, 0x34,                     //      Usage (Ry)
+        0x09, 0x35,                     //      Usage (Rz)
+        0x81, 0x02,                     //     Input (variable,absolute)
+        0xC0,                           //   End Collection
         0x15, 0x00,                     //   Logical Minimum (0)
         0x25, 0x07,                     //   Logical Maximum (7)
         0x35, 0x00,                     //   Physical Minimum (0)
         0x46, 0x3B, 0x01,               //   Physical Maximum (315)
         0x75, 0x04,                     //   Report Size (4)
-        0x95, 0x01,                     //   Report Count (1)
+        0x95, 0x04,                     //   Report Count (4)
         0x65, 0x14,                     //   Unit (20)
         0x05, 0x01,                     //   Usage Page (Generic Desktop)
         0x09, 0x39,                     //   Usage (Hat switch)
         0x81, 0x42,                     //   Input (variable,absolute,null_state)
-        0x05, 0x01,                     //   Usage Page (Generic Desktop)
-        0x09, 0x01,                     //   Usage (Pointer)
-        0xA1, 0x00,                     //   Collection ()
-        0x15, 0x00,                     //     Logical Minimum (0)
-        0x26, 0xFF, 0x03,               //     Logical Maximum (1023)
-        0x75, 0x0A,                     //     Report Size (10)
-        0x95, 0x04,                     //     Report Count (4)
-        0x09, 0x30,                     //     Usage (X)
-        0x09, 0x31,                     //     Usage (Y)
-        0x09, 0x32,                     //     Usage (Z)
-        0x09, 0x35,                     //     Usage (Rz)
-        0x81, 0x02,                     //     Input (variable,absolute)
-        0xC0,                           //   End Collection
-        0x15, 0x00,                     //   Logical Minimum (0)
-        0x26, 0xFF, 0x03,               //   Logical Maximum (1023)
-        0x75, 0x0A,                     //   Report Size (10)
-        0x95, 0x02,                     //   Report Count (2)
-        0x09, 0x36,                     //   Usage (Slider)
-        0x09, 0x36,                     //   Usage (Slider)
-        0x81, 0x02,                     //   Input (variable,absolute)
         0xC0                            // End Collection
 };
 #elif JOYSTICK_SIZE == 64
