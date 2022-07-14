72c3cc40ec194cb566ecb81bfd7251f34b5df2dd
Christopher Holley <torchtopher@gmail.com>
2022-07-14 14:09:25 -0400

    Add 8 bit axis

--- src/usb_joystick.h
+++ src/usb_joystick.h
@@ -27,6 +27,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 #ifndef USBjoystick_h_
 #define USBjoystick_h_
 
@@ -42,7 +43,7 @@ extern "C" {
 #endif
 void usb_joystick_configure(void);
 int usb_joystick_send(void);
-extern uint32_t usb_joystick_data[((JOYSTICK_SIZE+3)/4)];
+extern uint32_t usb_joystick_data[(JOYSTICK_SIZE+3)/4];
 extern volatile uint8_t usb_configuration;
 #ifdef __cplusplus
 }
@@ -55,6 +56,7 @@ class usb_joystick_class
         public:
         void begin(void) { }
         void end(void) { }
+
 #if JOYSTICK_SIZE == 12
 	void button(uint8_t button, bool val) {
 		if (--button >= 32) return;
@@ -63,51 +65,60 @@ class usb_joystick_class
 		if (!manual_mode) usb_joystick_send();
 	}
 	void X(unsigned int val) {
-		if (val > 1023) val = 1023;
-		usb_joystick_data[1] = (usb_joystick_data[1] & 0xFFFFC00F) | (val << 4);
+		if (val > 255) val = 255;
+		usb_joystick_data[1] = (usb_joystick_data[1] & 0xFFFFFF00) | (val);
 		if (!manual_mode) usb_joystick_send();
 	}
 	void Y(unsigned int val) {
-		if (val > 1023) val = 1023;
-		usb_joystick_data[1] = (usb_joystick_data[1] & 0xFF003FFF) | (val << 14);
-		if (!manual_mode) usb_joystick_send();
-	}
-	void position(unsigned int x, unsigned int y) {
-		if (x > 1023) x = 1023;
-		if (y > 1023) y = 1023;
-		usb_joystick_data[1] = (usb_joystick_data[1] & 0xFFF00000)
-			| (x << 4) | (y << 14);
+		if (val > 255) val = 255;
+		usb_joystick_data[1] = (usb_joystick_data[1] & 0xFFFF00FF) | (val << 8);
 		if (!manual_mode) usb_joystick_send();
 	}
 	void Z(unsigned int val) {
-		if (val > 1023) val = 1023;
+		if (val > 255) val = 255;
+		usb_joystick_data[1] = (usb_joystick_data[1] & 0xFF00FFFF) | (val << 16);
+		if (!manual_mode) usb_joystick_send();
+	}
+	void Xr(unsigned int val) {
+		if (val > 255) val = 255;
 		usb_joystick_data[1] = (usb_joystick_data[1] & 0x00FFFFFF) | (val << 24);
-		usb_joystick_data[2] = (usb_joystick_data[2] & 0xFFFFFFFC) | (val >> 8);
 		if (!manual_mode) usb_joystick_send();
 	}
-	void Zrotate(unsigned int val) {
-		if (val > 1023) val = 1023;
-		usb_joystick_data[2] = (usb_joystick_data[2] & 0xFFFFF003) | (val << 2);
+	void Yr(unsigned int val) {
+		if (val > 255) val = 255;
+		usb_joystick_data[2] = (usb_joystick_data[2] & 0xFFFFFF00) | (val);
 		if (!manual_mode) usb_joystick_send();
 	}
-	void sliderLeft(unsigned int val) {
-		if (val > 1023) val = 1023;
-		usb_joystick_data[2] = (usb_joystick_data[2] & 0xFFC00FFF) | (val << 12);
+	void Zr(unsigned int val) {
+		if (val > 255) val = 255;
+		usb_joystick_data[2] = (usb_joystick_data[2] & 0xFFFF00FF) | (val << 8);
 		if (!manual_mode) usb_joystick_send();
+		uint32_t *p = usb_joystick_data;
+		Serial.printf("Array %x %x %x %x", p[0], p[1], p[2], p[3]);
+
 	}
-	void sliderRight(unsigned int val) {
-		if (val > 1023) val = 1023;
-		usb_joystick_data[2] = (usb_joystick_data[2] & 0x003FFFFF) | (val << 22);
-		if (!manual_mode) usb_joystick_send();
-	}
-	void slider(unsigned int val) {
+
+	void Xrr(unsigned int val) {
 		if (val > 1023) val = 1023;
 		usb_joystick_data[2] = (usb_joystick_data[2] & 0x00000FFF)
 			| (val << 12) | (val << 22);
 		if (!manual_mode) usb_joystick_send();
 	}
-        inline void hat(int dir) {
-                uint32_t val = 0;
+	void Yrr(unsigned int val) {
+		if (val > 1023) val = 1023;
+		usb_joystick_data[2] = (usb_joystick_data[2] & 0x00000FFF)
+			| (val << 12) | (val << 22);
+		if (!manual_mode) usb_joystick_send();
+	}
+	void Zrr(unsigned int val) {
+		if (val > 1023) val = 1023;
+		usb_joystick_data[2] = (usb_joystick_data[2] & 0x00000FFF)
+			| (val << 12) | (val << 22);
+		if (!manual_mode) usb_joystick_send();
+	}
+        inline void hat(int num, int dir) {
+                uint32_t val = dir;
+                /*
                 if (dir < 0) val = 15;
                 else if (dir < 23) val = 0;
                 else if (dir < 68) val = 1;
@@ -117,10 +128,119 @@ class usb_joystick_class
                 else if (dir < 245) val = 5;
                 else if (dir < 293) val = 6;
                 else if (dir < 338) val = 7;
-		usb_joystick_data[1] = (usb_joystick_data[1] & 0xFFFFFFF0) | val;
+                */
+                uint32_t *p = usb_joystick_data;
+                switch(num) {
+		
+		  case 1: p[1] =  (p[1] & 0xFFFFFFF0) | (val << 0); break;
+		  case 2: p[1] =  (p[1] & 0xFFFFFF0F) | (val << 4); break;
+		  case 3: p[1] =  (p[1] & 0xFFFFF0FF) | (val << 8); break;
+		  case 4: p[1] =  (p[1] & 0xFFFF0FFF) | (val << 12); break;
+		  case 5: p[1] =  (p[1] & 0xFFF0FFFF) | (val << 16); break;
+		  case 6: p[1] =  (p[1] & 0xFF0FFFFF) | (val << 20); break;
+		  default:
+			return;
+		}
                 if (!manual_mode) usb_joystick_send();
         }
-#elif JOYSTICK_SIZE == 64
+
+
+#elif JOYSTICK_SIZE == 10
+	void button(unsigned int num, bool val) {
+		if (--num >= 32) return;
+		uint32_t *p = usb_joystick_data + (num >> 5);
+		num &= 0x1F;
+		if (val) *p |= (1 << num);
+		else *p &= ~(1 << num);
+		if (!manual_mode) usb_joystick_send();
+	}
+	void X(unsigned int val) {
+		if (val > 255) val = 255;
+		usb_joystick_data[1] = (usb_joystick_data[1] & 0xFFFFFF00) | (val);
+		if (!manual_mode) usb_joystick_send();
+	}
+	void Y(unsigned int val) {
+		if (val > 255) val = 255;
+		usb_joystick_data[1] = (usb_joystick_data[1] & 0xFFFF00FF) | (val << 8);
+		if (!manual_mode) usb_joystick_send();
+	}
+	void Z(unsigned int val) {
+		if (val > 255) val = 255;
+		usb_joystick_data[1] = (usb_joystick_data[1] & 0xFF00FFFF) | (val << 16);
+		if (!manual_mode) usb_joystick_send();
+	}
+	void Xr(unsigned int val) {
+		if (val > 255) val = 255;
+		usb_joystick_data[1] = (usb_joystick_data[1] & 0x00FFFFFF) | (val << 24);
+		if (!manual_mode) usb_joystick_send();
+	}
+	void Yr(unsigned int val) {
+		if (val > 255) val = 255;
+		usb_joystick_data[2] = (usb_joystick_data[2] & 0xFFFFFF00) | (val);
+		if (!manual_mode) usb_joystick_send();
+	}
+	void Zr(unsigned int val) {
+		if (val > 255) val = 255;
+		usb_joystick_data[2] = (usb_joystick_data[2] & 0xFFFF00FF) | (val << 8);
+		if (!manual_mode) usb_joystick_send();
+	}
+
+	void X16(unsigned int val) {
+		if (val > 0xFFFF) val = 0xFFFF;
+		usb_joystick_data[2] = (usb_joystick_data[2] & 0x0000FFFF) | (val << 16);
+		if (!manual_mode) usb_joystick_send();
+	}
+	void Y16(unsigned int val) {
+		if (val > 0xFFFF) val = 0xFFFF;
+		usb_joystick_data[3] = (usb_joystick_data[3] & 0xFFFF0000) | (val);
+		if (!manual_mode) usb_joystick_send();
+	}	
+	void Z16(unsigned int val) {
+		if (val > 0xFFFF) val = 0xFFFF;
+		usb_joystick_data[3] = (usb_joystick_data[3] & 0x0000FFFF) | (val << 16);
+		if (!manual_mode) usb_joystick_send();
+		uint32_t *p = usb_joystick_data;
+		Serial.printf("Array %x %x %x %x", p[0], p[1], p[2], p[3]);
+	}
+        inline void hat(unsigned int num, int angle) {
+		uint32_t val=15;
+		if (angle > 0 && angle < 23) val = 0;
+		else if (angle < 68) val = 1;
+		else if (angle < 113) val = 2;
+		else if (angle < 158) val = 3;
+		else if (angle < 203) val = 4;
+		else if (angle < 245) val = 5;
+		else if (angle < 293) val = 6;
+		else if (angle < 338) val = 7;
+		else if (angle < 360) val = 0;
+		uint32_t *p = usb_joystick_data;
+		switch(num) {
+		  case 1:
+			p[14] = (p[14] & 0xFFF0FFFF) | (val << 16); break;
+		  default:
+			return;
+		}
+		if (!manual_mode) usb_joystick_send();
+        }
+#endif
+	void useManualSend(bool mode) {
+		manual_mode = mode;
+	}
+	void send_now(void) {
+		usb_joystick_send();
+	}
+	private:
+	static uint8_t manual_mode;
+#if JOYSTICK_SIZE == 10
+	void analog16(unsigned int num, unsigned int value) {
+		if (value > 0xFFFF) value = 0xFFFF;
+		uint16_t *p = (uint16_t *)(&usb_joystick_data[4]);
+		p[num] = value;
+                if (!manual_mode) usb_joystick_send();
+	}
+#endif
+/*
+#elif JOYSTICK_SIZE == 36
 	void button(unsigned int num, bool val) {
 		if (--num >= 128) return;
 		uint32_t *p = usb_joystick_data + (num >> 5);
@@ -140,32 +260,25 @@ class usb_joystick_class
 		analog16(num + 6, position);
 	}
         inline void hat(unsigned int num, int angle) {
-		uint32_t val=angle;
+		uint32_t val=15;
+		if (angle > 0 && angle < 23) val = 0;
+		else if (angle < 68) val = 1;
+		else if (angle < 113) val = 2;
+		else if (angle < 158) val = 3;
+		else if (angle < 203) val = 4;
+		else if (angle < 245) val = 5;
+		else if (angle < 293) val = 6;
+		else if (angle < 338) val = 7;
+		else if (angle < 360) val = 0;
 		uint32_t *p = usb_joystick_data;
 		switch(num) {
 		  case 1:
-			p[14] = (p[14] & 0xFFF0FFFF) | (val << 16); break;
-		  case 2:
-			p[14] = (p[14] & 0xFF0FFFFF) | (val << 20); break;
-		  case 3:
-			p[14] = (p[14] & 0xF0FFFFFF) | (val << 24); break;
-		  case 4:
-			p[14] = (p[14] & 0x0FFFFFFF) | (val << 28); break;
-		  case 5:
-			p[15] = (p[15] & 0xFFFFFFF0) | (val << 0); break;
-		  case 6:
-			p[15] = (p[15] & 0xFFFFFF0F) | (val << 4); break;
-		  case 7:
-			p[15] = (p[15] & 0xFFFFF0FF) | (val << 8); break;
-		  case 8:
-			p[15] = (p[15] & 0xFFFF0FFF) | (val << 12); break;
-		  case 9:
 			p[15] = (p[15] & 0xFFF0FFFF) | (val << 16); break;
-		  case 10:
+		  case 2:
 			p[15] = (p[15] & 0xFF0FFFFF) | (val << 20); break;
-		  case 11:
+		  case 3:
 			p[15] = (p[15] & 0xF0FFFFFF) | (val << 24); break;
-		  case 12:
+		  case 4:
 			p[15] = (p[15] & 0x0FFFFFFF) | (val << 28); break;
 		  default:
 			return;
@@ -181,7 +294,7 @@ class usb_joystick_class
 	}
 	private:
 	static uint8_t manual_mode;
-#if JOYSTICK_SIZE == 64
+#if JOYSTICK_SIZE == 36
 	void analog16(unsigned int num, unsigned int value) {
 		if (value > 0xFFFF) value = 0xFFFF;
 		uint16_t *p = (uint16_t *)(&usb_joystick_data[4]);
@@ -189,6 +302,7 @@ class usb_joystick_class
                 if (!manual_mode) usb_joystick_send();
 	}
 #endif
+*/
 };
 extern usb_joystick_class Joystick;
 
